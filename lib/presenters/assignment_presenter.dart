762338c54878e44e401cc0e198b68b89526aeac7
Levi-010 <levibarnes4644@gmail.com>
2026-09-16 13:30:08 -0500

    Created the assignment_presenter.dart and finished it's first presenter.

---
+++ lib/presenters/assignment_presenter.dart
@@ -0,0 +1,17 @@
+import '../models/assignment_model.dart';
+
+class AssignmentPresenter {
+  final List<Assignment> _assignments = [];
+
+  List<Assignment> get assignments => _assignments;
+
+  void addAssignment(String title) {
+    if (title.trim().isNotEmpty) {
+      _assignments.add(Assignment(title: title));
+    }
+  }
+
+  void toggleCompleted(int index) {
+      _assignments[index].isCompleted = !_assignments[index].isCompleted;
+  }
+}
