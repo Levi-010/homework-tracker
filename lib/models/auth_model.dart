5b66e2357752a0e79346487afbe8cba0209f41cc
Levi-010 <levibarnes4644@gmail.com>
2026-09-23 13:45:35 -0500

    Finished creating auth_model.dart

---
+++ lib/models/auth_model.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthModel {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign in with email and password
+  Future<String?> login(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return null; // Return null if login is successful`
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  // Sign up with email and password
+  Future<String?> signUp(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      return null;
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  // Sign out
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  // Logs out current user
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  // Get current user
+  User? get currentUser => _auth.currentUser;
+  
+}
